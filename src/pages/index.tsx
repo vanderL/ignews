f80fdddbd0178b01de549950d4bd401aca038827
Vander Lima <vnd.vander@gmail.com>
2021-12-31 00:11:07 -0300

    API Routes in Next.js - Auth with Next-auth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
 import { stripe } from '../services/stripe';
@@ -13,7 +13,6 @@ interface HomeProps {
 }
 
 export default function Home({ product }: HomeProps) {
-  console.log(product);
   return (
     <>
       <Head>
@@ -38,7 +37,7 @@ export default function Home({ product }: HomeProps) {
 }
 
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1KCEmvIdc7vY11OkDb2YDy2m', {
     expand: ['product']
   })
@@ -55,7 +54,8 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product,
-    }
+    },
+    revalidate: 60 * 60 * 24, //24 hours
   }
 
 }
